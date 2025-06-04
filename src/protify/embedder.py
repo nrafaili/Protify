1d41535ab8362dcd113badcd2a0f69935275028c
Logan Hallee <lhallee@udel.edu>
2025-06-04 12:28:04 -0400

    Update embedder.py

--- src/protify/embedder.py
+++ src/protify/embedder.py
@@ -15,6 +15,22 @@ from pooler import Pooler
 from utils import torch_load, print_message
 
 
+def pool_parti(X: torch.Tensor, attentions: Tuple[torch.Tensor], attention_mask: torch.Tensor) -> torch.Tensor:
+    # X: (bs, seq_len, d)
+    # attentions: num_layres of (bs, n_heads, seq_len, seq_len)
+    # attention_mask: (bs, seq_len)
+    bs, seq_len, _ = X.shape
+    attentions = torch.stack(attentions, dim=1).float() # (bs, n_layers, n_heads, seq_len, seq_len)
+    att_mask = attention_mask[:, None, None, None, :].expand(bs, 1, 1, seq_len, seq_len)
+    attentions = attentions * att_mask
+    attentions = attentions.mean(dim=2) # (bs, n_layers, seq_len, seq_len)
+    attentions = attentions.mean(dim=1) # (bs, seq_len, seq_len)
+    attentions = attentions.mean(dim=-1) # (bs, seq_len)
+    X = X * attentions.unsqueeze(-1)
+    attention_mask = attention_mask.unsqueeze(-1)
+    return (X * attention_mask).sum(dim=1) / attention_mask.sum(dim=1) # (bs, d)
+
+
 def build_collator(tokenizer) -> Callable[[List[str]], tuple[torch.Tensor, torch.Tensor]]:
     def _collate_fn(sequences: List[str]) -> tuple[torch.Tensor, torch.Tensor]:
         """Collate function for batching sequences."""
