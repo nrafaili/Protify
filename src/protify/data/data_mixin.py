8cad03223ef274fc9674abf359afdf67ba11c204
Logan Hallee <lhallee@udel.edu>
2025-06-25 16:49:10 -0400

    update

--- src/protify/data/data_mixin.py
+++ src/protify/data/data_mixin.py
@@ -46,18 +46,24 @@ class DataArguments:
         self.max_length = max_length
         self.trim = trim
 
-        if data_names[0] == 'standard_benchmark':
-            self.data_paths = [supported_datasets[data_name] for data_name in standard_data_benchmark]
+        if len(data_names) > 0:
+            if data_names[0] == 'standard_benchmark':
+                self.data_paths = [supported_datasets[data_name] for data_name in standard_data_benchmark]
+            else:
+                self.data_paths = []
+                for data_name in data_names:
+                    if data_name in supported_datasets:
+                        self.data_paths.append(supported_datasets[data_name])
+                    else:
+                        print(f'{data_name} not found in supported datasets')
+                        print('We will attempt to load it from huggingface anyways, but this may not work')
+                        self.data_paths.append(data_name)
         else:
             self.data_paths = []
-            for data_name in data_names:
-                if data_name in supported_datasets:
-                    self.data_paths.append(supported_datasets[data_name])
-                else:
-                    self.data_paths.append(data_name)
         
         if data_dirs is not None:
             for dir in data_dirs:
+                print(dir)
                 if os.path.exists(dir):
                     self.data_paths.append(dir)
                 else:
@@ -262,7 +268,8 @@ class DataMixin:
             data_names.append(data_name)
 
         for data_dir in self.data_args.data_dirs:
-            data_name = data_dir.split('/')[-2]
+            # local_data/taxon
+            data_name = data_dir.split ('/')[-1]
             ppi = 'ppi' in data_dir.lower()
             train_path = glob(os.path.join(data_dir, 'train.*'))[0]
             valid_path = glob(os.path.join(data_dir, 'valid.*'))[0]
@@ -272,13 +279,31 @@ class DataMixin:
                 valid_set = read_excel(valid_path)
                 test_set = read_excel(test_path)
             else:
-                train_set = read_csv(train_path, delimiter=self._delimiter, names=self._col_names)
-                valid_set = read_csv(valid_path, delimiter=self._delimiter, names=self._col_names)
-                test_set = read_csv(test_path, delimiter=self._delimiter, names=self._col_names)
+                train_set = read_csv(train_path, delimiter=self._delimiter, names=self._col_names, header=0)
+                valid_set = read_csv(valid_path, delimiter=self._delimiter, names=self._col_names, header=0)
+                test_set = read_csv(test_path, delimiter=self._delimiter, names=self._col_names, header=0)
 
             train_set = Dataset.from_pandas(train_set)
             valid_set = Dataset.from_pandas(valid_set)
             test_set = Dataset.from_pandas(test_set)
+
+            if not ppi:
+                print('Standardizing column names')
+                seq_col = self.data_args.col_names[0]
+                label_col = self.data_args.col_names[1]
+                train_set = train_set.rename_columns({seq_col: 'seqs', label_col: 'labels'})
+                valid_set = valid_set.rename_columns({seq_col: 'seqs', label_col: 'labels'})
+                test_set = test_set.rename_columns({seq_col: 'seqs', label_col: 'labels'})
+                # drop everything else
+                print('Removing extras')
+                train_set = train_set.remove_columns([col for col in train_set.column_names if col not in ['seqs', 'labels']])
+                valid_set = valid_set.remove_columns([col for col in valid_set.column_names if col not in ['seqs', 'labels']])
+                test_set = test_set.remove_columns([col for col in test_set.column_names if col not in ['seqs', 'labels']])
+
+            print(train_set['seqs'][0])
+            print(train_set['labels'][0])
+            print(valid_set)
+            print(test_set)
             datasets.append((train_set, valid_set, test_set, ppi))
             data_names.append(data_name)
 
